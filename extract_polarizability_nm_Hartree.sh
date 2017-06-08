cd27f2aabc2ac76f557821004df325fc45016e0c
ANKIT RAJ <araj7540@gmail.com>
2017-06-08 21:12:24 +0800

    Create extract_polarizability_nm_Hartree.sh

---
+++ extract_polarizability_nm_Hartree.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# to extract the values of the polarizability for different wavelengths in Hartree from output file and save as txt file
+# with cols as freq(hartree), freq(nm), alpha_zz, alpha_xx
+nlines="$1" # number of lines of the polarizability calculated (number of freqs)
+of="$2"  # .out file =>> output file
+desc="$3" # description here for file name (will be used for the file name)
+
+dir=$(pwd)
+
+#rm nm t1 t2 t3
+
+grep -A"$nlines"   "ZDIPLEN  (unrel.)  0.0000   ZDIPLEN  (unrel.)" $of  |  awk '{print $2}' | tail -"$nlines"  >   t1
+
+while read p; do
+q=$p
+nm=`echo "(10000000/($q*219474.6313702))" |bc -l` # conversion to nm
+
+#echo $q , $nm
+echo -e $nm >> nm
+done <t1
+
+
+grep -A"$nlines"   "ZDIPLEN  (unrel.)  0.0000   ZDIPLEN  (unrel.)"  $of  |  awk '{print $3}' | tail -"$nlines"  >  t2
+
+grep -A"$nlines"    "XDIPLEN  (unrel.)  0.0000   XDIPLEN  (unrel.)"  $of  |  awk '{print $3}' | tail -"$nlines"  > t3
+
+now="$(date)"
+#printf "Current time %s\n" "$now"
+now="$(date +'%D----%T')"
+
+echo "$now"
+filename=${desc}
+echo "$filename"".txt will be generated in a folder called result".
+paste -d'\t' nm t1 t2 t3 >   temp
+mv temp $filename.txt
+
+sed -i '1s/^/nm Hartree ZZ      XX\n/' $filename.txt
+mv $filename.txt  result/.
+rm t1 t2 t3 nm
