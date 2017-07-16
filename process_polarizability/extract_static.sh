3dd307c86cf9bec716a75c1a35eda95614f6dfe1
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2017-07-16 18:29:52 +0800

    Update extract_static.sh

--- process_polarizability/extract_static.sh
+++ process_polarizability/extract_static.sh
@@ -5,13 +5,15 @@ echo "Please enter the file name (without number): "
 read fname
 echo "You entered: $iv, $fname" ;
 #---------------------------------------------------------------------------------
-rm -f g_distance.txt
-rm -f tempx, tempz
+rm  g_distance.txt
+rm  tempx  tempz   static_zz.txt  static_xx.txt
 rm -rf output_0
+
 mkdir output_0
+
 i=1 ; lim=$(($iv+1))
 while [ $i -lt $lim ] ;  do
-        #echo $fname$i.out
+        echo $i
         grep   "ZDIPLEN  (unrel.)  0.0000   ZDIPLEN  (unrel.)  0.0000"  $fname$i.out | awk '{print $7}' > tempz
         cat tempz >> static_zz.txt
 
@@ -23,13 +25,14 @@ while [ $i -lt $lim ] ;  do
         echo $r_value >> g_distance.txt
         i=$(($i+1))
 done
-rm tempx, tempz
+rm tempx tempz
 echo "$(($i-1))"_processed
-cp g_distance.txt output_0/g_distance.txt
 
 sed -i -e '1idistance\' g_distance.txt
-sed -i -e '1idistance\' output_0/g_distance.txt
+sed -i -e '1ixx\'  static_xx.txt
+sed -i -e '1izz\'  static_zz.txt
 
+cp g_distance.txt output_0/g_distance.txt
 cp static_xx.txt static_zz.txt  output_0/.
 echo "Static polarizability generated as 1-D array along distance."
 echo "--end--"
