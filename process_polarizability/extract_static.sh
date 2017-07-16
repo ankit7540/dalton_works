5cf6bd7cdb8ac8a7008a0b5d783e71bd95fdc1a6
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2017-07-16 18:18:30 +0800

    Update extract_static.sh

--- process_polarizability/extract_static.sh
+++ process_polarizability/extract_static.sh
@@ -6,24 +6,30 @@ read fname
 echo "You entered: $iv, $fname" ;
 #---------------------------------------------------------------------------------
 rm -f g_distance.txt
+rm -f tempx, tempz
 rm -rf output_0
-rm static0.txt
-#---------------------------------------------------------------------------------
 mkdir output_0
 i=1 ; lim=$(($iv+1))
 while [ $i -lt $lim ] ;  do
         #echo $fname$i.out
-        grep   "ZDIPLEN  (unrel.)  0.0000   ZDIPLEN  (unrel.)  0.0000"  $fname$i.out | awk '{print $7}' > temp0
-        cat temp0 >> static0.txt
+        grep   "ZDIPLEN  (unrel.)  0.0000   ZDIPLEN  (unrel.)  0.0000"  $fname$i.out | awk '{print $7}' > tempz
+        cat tempz >> static_zz.txt
+
+        grep   "XDIPLEN  (unrel.)  0.0000   XDIPLEN  (unrel.)  0.0000"  $fname$i.out | awk '{print $7}' > tempx
+        cat tempx >> static_xx.txt
 
         text=$(awk '/H2     0.000000     0.000000/' $fname$i.out)
         r_value=$(echo $text | awk '{print $4}')
         echo $r_value >> g_distance.txt
         i=$(($i+1))
 done
-rm temp0
+rm tempx, tempz
 echo "$(($i-1))"_processed
 cp g_distance.txt output_0/g_distance.txt
-cp static0.txt  output_0/static0.txt
-echo "1-d array of static polarizability vs r is generated."
+
+sed -i -e '1idistance\' g_distance.txt
+sed -i -e '1idistance\' output_0/g_distance.txt
+
+cp static_xx.txt static_zz.txt  output_0/.
+echo "Static polarizability generated as 1-D array along distance."
 echo "--end--"
