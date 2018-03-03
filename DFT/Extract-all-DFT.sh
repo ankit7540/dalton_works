ccadd967cca06f390390383473949e1c49aa7f02
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2018-03-03 22:32:56 +0800

    updated code for beta

--- DFT/Extract-all-DFT.sh
+++ DFT/Extract-all-DFT.sh
@@ -22,44 +22,79 @@ while [ "$i" -le "$n" ]
 do
 k=-2
 # echo $i
-        mkdir  property/mode_$i
-        while [ "$k" -le 2 ]
-        do
-        # echo "$fname"_"$i"_"$k".out
-        ##############  Hyperpolarizability ##############
-	grep -A 110  "DFT-QR computed in a linearly-scaling fashion." "$file" > temp
-	tail -109 temp > temp2
+    mkdir  property/mode_$i
+    while [ "$k" -le 2 ]
+    do
+    #echo "$fname"_"$i"_"$k".out
+    file = $(echo "$fname"_"$i"_"$k".out)
+    echo "$file"
+    ##############  Hyperpolarizability ##############
+    grep -A 110  "DFT-QR computed in a linearly-scaling fashion." "$file" > temp
+    tail -109 temp > temp2
 
-	awk '{print $8 $9 $10}' temp2 > temp3
+    awk '{print $8 $9 $10}' temp2 > temp3
 
-	sed -i 's/;/,/g' temp3
-	sed -i 's/X/0/g' temp3
-	sed -i 's/Y/1/g' temp3
-	sed -i 's/Z/2/g' temp3
+    sed -i 's/;/,/g' temp3
+    sed -i 's/X/0/g' temp3
+    sed -i 's/Y/1/g' temp3
+    sed -i 's/Z/2/g' temp3
 
-	# square brackets for python matrix assignment
-	sed -i 's/(/[/g' temp3
-	sed -i 's/)/]/g' temp3
-	sed -i 's/,/][/g' temp3
+    # square brackets for python matrix assignment
+    sed -i 's/(/[/g' temp3
+    sed -i 's/)/]/g' temp3
+    sed -i 's/,/][/g' temp3
 
-	# post processing by python script to generate 3 matrices having the beta values.
-	# the python script is generated dynamically to process the data.
-	# three 3x3 matrices are extracted for Igor output.
+    # next write up a python script to set up matrix 3x3x3 from which
+    # three 3x3 matrices are extracted for Igor output.
 
-	echo "# python script : generate hyperpolarizability tensor and also three matrices" > process_beta.py
-	echo "import numpy as np" >> process_beta.py
-	echo "beta=np.zeros((3,3,3))" >> process_beta.py
-	cat temp3 >> process_beta.py
-	cat rank3tensor_split.py >> process_beta.py
-	python process_beta.py	# execute the script
+    echo "# python script : generate hyperpolarizability tensor and also three matrices" > process_beta.py
+    
+    echo "import numpy as np" >> process_beta.py
 
-        rm temp temp2 temp3
-	# post processing to be done later to produce the itx file.
-	exit 0 
+    echo "beta=np.zeros((3,3,3))" >> process_beta.py
+    cat temp3 >> process_beta.py
 
+    echo "print(beta)" >> process_beta.py
+
+    #-------------------------------------------------
+
+    # post processing  the beta tensor 
+
+    cat rank3tensor_split.py >> process_beta.py
+    python process_beta.py
+
+    # generating the itx files
+    sed -i '1s/^/IGOR\n/' b0.txt
+    sed -i '2s|^|WAVES /D /N=(3,3) b'"$i"0"$k"'\n|' "b0.txt"
+    sed -i '3s/^/BEGIN\n/' b0.txt
+    echo -e 'END' >> b0.txt
+    sed -i "2 s/b/'b/;2 s/$/'/" b0.txt
+    cat b0.txt > property/mode_$i/b"$i"0"$k".itx
+
+
+    sed -i '1s/^/IGOR\n/' b1.txt
+    sed -i '2s|^|WAVES /D /N=(3,3) b'"$i"1"$k"'\n|' "b1.txt"
+    sed -i '3s/^/BEGIN\n/' b1.txt
+    echo -e 'END' >> b1.txt
+    sed -i "2 s/b/'b/;2 s/$/'/" b1.txt
+    cat b1.txt > property/mode_$i/b"$i"1"$k".itx
+
+
+    sed -i '1s/^/IGOR\n/' b2.txt
+    sed -i '2s|^|WAVES /D /N=(3,3) b'"$i"2"$k"'\n|' "b2.txt"
+    sed -i '3s/^/BEGIN\n/' b2.txt
+    echo -e 'END' >> b2.txt
+    sed -i "2 s/b/'b/;2 s/$/'/" b2.txt
+    cat b2.txt > property/mode_$i/b"$i"2"$k".itx
+
+    rm b0.txt b1.txt b2.txt
+    rm temp temp2 temp3 
+
+    #####################################################################################
         echo ""
         # Polarizability
-	    ./Extract-DFT-polarizability
+	    sh ./Extract-DFT-polarizability "$file"
+        # output will be generated in polarizability.dat
 
         # Dipole moment part of output exported to a temp file (for DFT, same as CCSD)
 	grep -A 10 "Dipole moment components" "$fname"_"$i"_"$k".out > muTemp 
