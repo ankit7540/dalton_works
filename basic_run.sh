54d6776b6d7e01363986eca52cedd64254b0d861
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2019-11-23 14:08:01 +0800

    restructured : keep all editable variables together

--- basic_run.sh
+++ basic_run.sh
@@ -2,14 +2,39 @@
     # Running QM-JOB with given dal and mol files present the current folder.
         d="$1"  # .dal file
         m="$2"  # .mol file
-        mem="$3" # memory allocated for this calculation
+        mem="$3" # memory allocated for this calculation (in MegaBytes)
         cores="$4" # CPU cores for this calculation
 
     dir=$(pwd) # get present directory
     dt=$(date  +%Y-%m-%d:%H:%M:%S )
-    echo -e 'Job started @ '$dt'' >> /home/<user>/dalton/runlog.log  # Change address as needed.
+
+    ##################################################################
+    # EDIT FOLLOWING BLOCK 
+
+    log_path=<path to the log file>
+    # example : log_path=~/calc_logs/dalton_runlog.log
     
-    cd /home/<user>/ChemPackage/dalton_mod/dalton # custom modified installation
+    dalton_ins=<path to DALTON installation directory>
+    # example : dalton_ins=~/ChemPackage/dalton_mod/dalton
+    
+    scratch=<path to scratch disk>
+    # example : dalton_ins=/mnt/raid0/scratch
+    
+    basis_folder=<path to folder containing basis>
+    #example : basis_folder=~/dalton/ExtBasis
+    
+    compiler_path1=/opt/intel/compilers_and_libraries_2017.0.098/linux/bin/compilervars.sh
+    compiler_arg=intel64
+    
+    compiler_path2=/opt/intel/mkl/bin/mklvars.sh
+    compiler_arg=intel64    
+    
+    
+    ##################################################################
+    
+    echo -e 'Job started @ '$dt'' >> $log_path  # log the start of job
+    
+    cd $dalton_ins # custom modified installation
     echo "-----------------------------------------------"
     df -h /dev/md0
     echo "-----------------------------------------------"
@@ -21,15 +46,15 @@
     
 
     echo "-----------------------------------------------"
-    export DALTON_TMPDIR=/mnt/raid0/scratch
+    export DALTON_TMPDIR=$scratch
     export OMP_NUM_THREADS=$cores
-    source /opt/intel/compilers_and_libraries_2017.0.098/linux/bin/compilervars.sh intel64
-    source /opt/intel/mkl/bin/mklvars.sh intel64
+    source $compiler_path1 $compiler_arg
+    source $compiler_path2 $compiler_arg
 
     echo "//-------process started----------------------------//"
     dt1=$(date '+%d/%m/%Y %H:%M:%S');
     #following line calls the program and runs the job.
-    ./dalton -b ~/dalton/ExtBasis   -w  "$dir"    -mb $mem $d $m
+    ./dalton -b $basis_folder   -w  "$dir"    -mb $mem   $d   $m
 
     dt2=$(date '+%d/%m/%Y %H:%M:%S');
  # Give path to the log file in the string below. Details of the calculation will be saved there.
