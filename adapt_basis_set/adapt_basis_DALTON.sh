74febbc24bcf3e3298a81c9340f92a3fe116a6b8
Ankit Raj <ankit7540.ipm02g@nctu.edu.tw>
2019-12-15 22:25:50 +0800

    updated the format output

--- adapt_basis_set/adapt_basis_DALTON.sh
+++ adapt_basis_set/adapt_basis_DALTON.sh
@@ -15,35 +15,44 @@ file="$1"
         atom=$( echo $str | awk  '{print $2}' )
         echo "ATOM : " $atom "Basis : " $basis;
 
-	# name of output file (consists of basis name and atom)
+        # name of output file (consists of basis name and atom)
         output="mod_"$basis"_"$atom
         echo "output file : "$output;
 
         # commands for modifying the text in the file
-	sed '/^!/ d' "$file" > temp1
+        sed '/^!/ d' "$file" > temp1
         sed 's/H/f /g' temp1 > temp2
-        sed 's/0.0000000/0.0/g' temp2 > temp3
+        #sed 's/0.0000000/0.0/g' temp2 > temp3
+        #sed -i 's/1.0000000/1.0/g' temp3
+        #sed -i 's/.000000/.0/g' temp3
+        #sed -i 's/000/0/g' temp3
+        #sed -i 's/0.0/0/g' temp3
+        #sed -i 's/00/0/g' temp3
+        sed  's/0.000000E+00/0/g' temp2 >  temp3
+        sed -i 's/1.000000E+00/1.0/g' temp3
+        sed -i 's/0.00000000/0.0/g' temp3
+        sed -i 's/1.00000000/1.0/g' temp3
         sed -i 's/1.0000000/1.0/g' temp3
-        sed -i 's/.000000/.0/g' temp3
+
         sed -i 's/              /  /g' temp3
         sed "s/^[ \t]*//" -i temp3
         sed -i 's/    / /g' temp3
 
-	# remove the line with alphabet a
+        # remove the line with alphabet a
         sed -e '/^a/ d' temp3  >  "$output"
-	
-	# remove empty lines
+
+        # remove empty lines
         sed -i '/^[[:space:]]*$/d'  "$output"
-	
-	# delete empty lines
+
+        # delete empty lines
         rm temp1  temp2 temp3
-	
+
         echo "-------------- line content and character count ----------------"
 
         cat  "$output"  | while read line
         do
-	        count=$(echo $line | wc -c)
-        	echo $line  "  -  "  $count
+                count=$(echo $line | wc -c)
+                echo $line  "  -  "  $count
         done
-	echo -e "\n Output file :  $output   generated. \n"
+        echo -e "\n Output file :  $output   generated. \n"
 ######################################################################################
